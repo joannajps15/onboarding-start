8b9e1c2018b43d29b95108740d02f235ed39b0dc
Joanna Sebastiampillai <j4sebast@uwaterloo.ca>
2025-09-20 18:30:45 -0400

    updates to spi_peripheral

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -25,8 +25,8 @@ module spi_peripheral (
     reg [1:0] r_ncs;
     reg [1:0] r_copi;
 
-    always @ (posedge clk) begin
-        if (rst_n) begin
+    always @ (posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
             r_sclk[2:0] <= 3'b000;
             r_ncs[1:0] <= 2'b00;
             r_copi[1:0] <= 2'b00;
@@ -47,53 +47,58 @@ module spi_peripheral (
     end
 
     localparam max_address = 7'h04;
-    reg [4:0] count = 0; //(3) BIT COUNTING
-    reg [15:0] temp = 0; //data temp
+    reg [4:0] count; //(3) BIT COUNTING
+    reg [15:0] temp; //data temp
     wire high = ~r_sclk[2] & r_sclk[1];
-    wire count_enable = 1;
-
-    always @ (posedge clk) begin
-        if (rst_n) begin
+    wire low = r_sclk[2] & ~r_sclk[1];
+    reg count_enable;
+    
+    // CAPTURE 16-BITS
+    always @ (posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
             //reset signals
             count_enable <= 1;
             count <= 0;
+            temp <= 0;      
+        end else if (~r_ncs[1]) begin
+            //get values
+            if (count_enable && high) begin
+                temp[15-count] <= r_copi[1];
+                count <= count + 1;
+                count_enable <= 0;
+            end else if (low) begin
+                count_enable <= 1;
+            end
+        end else begin
+            //reset signals
+            count_enable <= 1;
             temp <= 0;
+            count <= 0;
+        end 
+    end
+
+    always @ (posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            //reset signals
             en_reg_out_7_0 <= 0;
             en_reg_out_15_8 <= 0;
             en_reg_pwm_7_0 <= 0;
             en_reg_pwm_15_8 <= 0;
-            pwm_duty_cycle <= 0;            
+            pwm_duty_cycle <= 0;      
         end else begin 
-            if (~r_ncs[1]) begin
-                //get values
-                if (count_enable && ~r_sclk[2] & r_sclk[1]) begin
-                    temp[15-count] <= r_copi[1];
-                    count <= count + 1;
-                    count_enable <= 0;
-                end else if (r_sclk[2] & ~r_sclk[1]) begin
-                    count_enable <= 1;
-                end
-            end else begin
-                //reset signals
+            if ((count == 5'd16) && (temp[15] == 1) && (temp[14:8] <= max_address)) begin
+                case (temp[14:8])
+                    7'h00: en_reg_out_7_0 <= temp[7:0];
+                    7'h01: en_reg_out_15_8 <= temp[7:0];
+                    7'h02: en_reg_pwm_7_0 <= temp[7:0];
+                    7'h03: en_reg_pwm_15_8 <= temp[7:0];
+                    7'h04: pwm_duty_cycle <= temp[7:0];
+                endcase                
+                //finished processing: reset signals            
                 count_enable <= 1;
                 temp <= 0;
                 count <= 0;
             end
-
-            if (count == 5'd16) begin
-                if ((temp[15] == 1) && (temp[14:8] <= max_address)) begin
-                    case (temp[14:8])
-                        7'h00: en_reg_out_7_0 <= temp[7:0];
-                        7'h01: en_reg_out_15_8 <= temp[7:0];
-                        7'h02: en_reg_pwm_7_0 <= temp[7:0];
-                        7'h03: en_reg_pwm_15_8 <= temp[7:0];
-                        7'h04: pwm_duty_cycle <= temp[7:0];
-                    endcase
-                end
-                count <= 0;
-                temp <= 0;
-            end 
-            
         end
     end
 

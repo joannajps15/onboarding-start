47065d5286a90caf38d7312ee6b5b3f940c02396
Joanna Sebastiampillai <j4sebast@uwaterloo.ca>
2025-09-27 01:22:54 -0400

    refined spi_peripheral

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -3,12 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// STEPS TO DEBUG
-
-// all bit-widths are correctly declared
-// negedge rst_n might not be supported?
-// width mismatches
-
 `default_nettype none
 
 module spi_peripheral (
@@ -50,18 +44,17 @@ module spi_peripheral (
         end
     end
 
+    // PROCESS SIGNALS
+
     localparam max_address = 7'h04;
-    reg [4:0] count; //(3) BIT COUNTING
+    reg [4:0] count; //bit counting
     reg [15:0] temp; //data temp
-    wire high = ~r_sclk[2] & r_sclk[1];
-    wire low = r_sclk[2] & ~r_sclk[1];
-    reg count_enable;
+    wire high = ~r_sclk[2] & r_sclk[1]; //edge detection
 
     // CAPTURE 16-BITS
     always @ (posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             //reset signals
-            count_enable <= 1;
             count <= 0;
             temp <= 0;      
             
@@ -71,30 +64,25 @@ module spi_peripheral (
             en_reg_pwm_15_8 <= 0;
             pwm_duty_cycle <= 0;    
         end else if (~r_ncs[1]) begin
-            //get values
+            //active-low chip select is set - get values
             if (count_enable && high) begin
                 temp[15-count] <= r_copi[1];
                 count <= count + 1;
-                count_enable <= 0;
-            end else if (low) begin
-                count_enable <= 1;
-            end
-        end else if ((count == 5'd16) && (temp[15] == 1) && (temp[14:8] <= max_address)) begin
-                case (temp[14:8])
-                    7'h00: en_reg_out_7_0 <= temp[7:0];
-                    7'h01: en_reg_out_15_8 <= temp[7:0];
-                    7'h02: en_reg_pwm_7_0 <= temp[7:0];
-                    7'h03: en_reg_pwm_15_8 <= temp[7:0];
-                    7'h04: pwm_duty_cycle <= temp[7:0];
-                    default: ;
-                endcase                
-                //finished processing: reset signals            
-                count_enable <= 1;
-                temp <= 0;
-                count <= 0;
+        end else if ((count == 5'd16) && (temp[15]) && (temp[14:8] <= max_address)) begin
+            //determine output  
+            case (temp[14:8])
+                7'h00: en_reg_out_7_0 <= temp[7:0];
+                7'h01: en_reg_out_15_8 <= temp[7:0];
+                7'h02: en_reg_pwm_7_0 <= temp[7:0];
+                7'h03: en_reg_pwm_15_8 <= temp[7:0];
+                7'h04: pwm_duty_cycle <= temp[7:0];
+                default: ;
+            endcase                
+            //finished processing: reset signals  
+            temp <= 0;
+            count <= 0;
         end else begin
             //reset signals
-            count_enable <= 1;
             temp <= 0;
             count <= 0;
         end 
